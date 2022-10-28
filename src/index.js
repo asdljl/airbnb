8856ab7608f2c404b9962a9fbae662b208772c13
lijlds <1418573820@qq.com>
2022-10-28 20:16:46 +0800

    配置项目路由

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from "react-router-dom";
+import {Suspense} from "react";
+
 import App from './App';
 import "normalize.css"
 import "./assets/css/index.less"
@@ -7,6 +10,11 @@ import "./assets/css/index.less"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback="loading">
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Suspense>
+
   </React.StrictMode>
 );
