0c210abe09396a2659633c73c6be3d9003b26ef9
lijlds <1418573820@qq.com>
2022-10-28 16:31:43 +0800

    重置css样式

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,16 @@
 const path = require("path");
+const CracoLessPlugin = require('craco-less')
 
 const resolve =pathname => path.resolve(__dirname,pathname)
 
 module.exports = {
+  // less配置
+  plugin: [
+    {
+      plugin: CracoLessPlugin
+    }
+  ],
+  // webpack配置
   webpack: {
     alias: {
       "@": resolve("src"),
